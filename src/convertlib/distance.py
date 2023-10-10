959c62874363eede173a8ba40dbef704d497a3c0
Nathan Simpson <nsimpson@turing.ac.uk>
2023-10-10 17:00:34 +0100

    unholy commit

--- src/convertlib/distance.py
+++ src/convertlib/distance.py
@@ -1,5 +1,6 @@
 import numpy as np
 
 # converter from miles to kilometers
-def miles_to_km(miles):
-    return np.asarray(miles) * 1.60934
+def miles_to_km(            miles):
+    return np.asarray(
+        miles) *  1.60934
